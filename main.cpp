1a345c647183db27e037479445448b61302aa718
Carlos Fisac Ferrández <charles8ff@gmail.com>
2023-03-29 17:01:16 +0200

    Craft homework

--- main.cpp
+++ main.cpp
@@ -8,10 +8,27 @@
 
 using namespace std;
 
+
+bool isItPalindrome(string s, int i){
+    // Clarity purposes:
+    cout << "Evaluating: \"" << s[i] << "\" and \""<< s[ s.size()-i-1 ]<<"\""<<endl;
+
+    if( i > s.size()/2) return true;
+    // else
+    return s[ i ] == s[ s.size()-i-1 ] && isItPalindrome(s, i+1);
+}
+
 int main (){
 
 
-    cout<< "helloo" <<endl;
+    string myIntput;
+    cout << "Introduza una palabra a deterimar:\n\t>>";
+    getline(cin, myIntput);
+
+    if (isItPalindrome(myIntput, 0))
+        cout << "Sí, es un palíndromo." << endl;
+    else
+        cout << "No, no es un palíndromo." << endl;
 
     return 0;
 }
